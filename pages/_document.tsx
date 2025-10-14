284cb75e04dd5ba5fc245a5500e031ac7aadcd95
presell <122242589+presell@users.noreply.github.com>
2025-10-14 16:31:27 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* Start in 'prehydration' state so CSS can hide the drawer before JS */}
       <body className="prehydration">
         <Main />
         <NextScript />
